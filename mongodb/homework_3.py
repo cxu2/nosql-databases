38962d243c397dd1f2dc2151051142c046b25b26
Chengtian <cxu2@bates.edu>
2018-04-17 17:14:24 -0400

    homework3 done

---
+++ mongodb/homework_3.py
@@ -0,0 +1,72 @@
+from pymongo import MongoClient
+from pprint import pprint
+
+client = MongoClient("mongodb://localhost")
+db = client.movie
+
+# Part A
+db.movies.update_many(
+    {"rated": "NOT RATED", "genres": "Drama"},
+    {"$set": {"rated": "Pending rating"}}
+)
+
+# Part B
+db.movies.insert_one(
+    { "title" : "The Shape of Water",
+      "year" : 2017,
+      "countries" : ["USA"],
+      "genres" : ["Adventure", 
+                  "Drama",
+                  "Fantasy",
+                  "Horror",
+                  "Romance",
+                  "Thriller"
+                 ],
+      "directors" : ["Guillermo del Toro"],
+      "imdb" : { "id" : 5580390,
+                 "rating" : 7.5,
+                 "votes" : 194478
+               }
+    }        
+)
+
+# Part C
+cursor1 = db.movies.aggregate(
+    [ 
+        { "$match" : { "genres" : "Drama" } },
+        { "$group" : { "_id" : "Drama", "count" : { "$sum" : 1 } } }
+    ]
+)
+
+for document in cursor1:
+    pprint(document)
+
+# Part D
+cursor2 = db.movies.aggregate(
+    [
+        { "$match" : { "countries" : "China", "rated" : "Pending rating" } },
+        { "$group" : { "_id" : { "country" : "China", "rating" : "Pending rating" }, "count" : { "$sum" : 1 } } }
+    ]
+)
+
+for document in cursor2:
+    pprint(document)
+
+# Part E
+db.teams.insert([
+    { "_id" : 1, "name" : "Houston Rockets", "wins" : 65, "loses" : 17 },
+    { "_id" : 2, "name" : "Toronto Raptors", "wins" : 59, "loses" : 23 }
+])
+
+db.allstars.insert([
+    { "_id" : 1, "names" : ["Stephen Curry", "Kevin Durant", "Klay Thompson", "Draymond Green"], "team" : "Golden State Warriors" },
+    { "_id" : 2, "names" : ["Demar DeRozan", "Kyle Lowry"], "team" : "Toronto Raptors" },
+    { "_id" : 3, "names" : ["James Harden"], "team" : "Houston Rockets" }
+])
+
+cursor3 = db.teams.aggregate([
+    { "$lookup" : { "from" : "allstars", "localField" : "name", "foreignField" : "team", "as" : "allstars"} }    
+])
+
+for document in cursor3:
+    pprint(document)
